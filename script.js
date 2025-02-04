7e0c18537e005a8e66b08b12dee35e68b8712ea1
jaysmart98 <ogunbunmijoshua60@gmail.com>
2025-02-04 11:45:10 +0100

    added my files

---
+++ script.js
@@ -0,0 +1,34 @@
+function addItem() {
+if(item.value ==''){
+    // allItems.push(item.value)
+    Toastify({
+        text: "Input can not be empty",
+        duration: 3000,
+        newWindow: true,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "left", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "linear-gradient(to right, #00b09b, #96c93d)",
+        },
+        onClick: function(){} // Callback after click
+      }).showToast();
+}
+else{
+    Toastify({
+    text: `${item.value} added successfully`,
+    duration: 3000,
+    destination: "https://github.com/apvarun/toastify-js",
+    newWindow: true,
+    close: true,
+    gravity: "top", // `top` or `bottom`
+    position: "left", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      background: "linear-gradient(to right, #00b09b, #96c93d)",
+    },
+    onClick: function(){} // Callback after click
+  }).showToast();
+ }
+}
